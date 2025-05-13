84f9f69c531c0e0f99aa554f3d0e07c3ada410e8
harishhbaskar <harishhbaskar@gmail.com>
2025-05-13 13:39:29 +0530

    message

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Header from './Header.jsx'
+import Body from './Body.jsx'
+
+export default function App(){
+    return(
+        <>
+            <Header/>
+            <Body/>
+        </>
+    );
+    
+}
